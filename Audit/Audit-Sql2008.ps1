ba8922c06546b9e557ae2460d29843efc402575f
Dallas Moore <Dallas.moore21@gmail.com>
2014-01-21 11:18:22 -0500

    Create Audit-Sql2008.ps1

---
+++ Audit/Audit-Sql2008.ps1
@@ -0,0 +1,25 @@
+<#
+###################################
+# Author:         Dallas Moore    #
+# Date created:   Jan 2014        #
+# Last updated:   Jan 2014        #
+# Ver:            1.0             #
+###################################
+
+.SYNOPSIS   
+	This script audits a SQL Server 2008 instance against the CIS Benchmark for SQL Server 2008
+    
+.DESCRIPTION 
+	This is a cheap translation a of SQL script written by David Lau, but it works!  
+
+.EXAMPLES   
+	Basic usage:
+    .\Audit-SQL2008.ps1 -InstanceName INFOSEC-VM-2012\SEKRETSQUIRREL
+    
+.PARAMS
+    InstanceName
+        Use this parameter to specify the instance name that is being audited
+
+.NOTE
+    This script is still being written.  This space will be updated when the script is finished.
+#>
