10d70853370e73134b96cf07fa9b6d58b866d501
Dallas Moore <Dallas.moore21@gmail.com>
2016-08-16 16:23:40 -0500

    Update
    Included loaded module paths

--- Better-Netstat.ps1
+++ Better-Netstat.ps1
@@ -1,5 +1,3 @@
-#requires -Version 2
-
 $netstats = NETSTAT.EXE -anop tcp | Select-Object -Skip  4 |
 ForEach-Object -Process {[regex]::replace($_.trim(),'\s+',' ')} |
 ConvertFrom-Csv -delimiter ' ' -Header 'proto', 'src', 'dst', 'state', 'pid' |
@@ -8,7 +6,9 @@ Select-Object -Property pid, proto, dst, src, state,
     @{name = 'session_id'; expression = {(Get-Process -id $_.pid).sessionid}},
     @{name = 'window_title'; expression = {(Get-Process -id $_.pid).mainwindowtitle}},
     @{name = 'ppid'; expression = {(gwmi win32_process -Filter "processid='$($_.pid)'").parentprocessid}},
-    @{name = 'gpid'; expression = {(gwmi win32_process -Filter "processid='$($_.ppid)'").parentprocessid}},
-    @{name = 'parent_process_name'; expression = {(Get-Process -id $_.ppid).name}}
+    #@{name = 'gpid'; expression = {(gwmi win32_process -Filter "processid='$($_.ppid)'").parentprocessid}},
+    #@{name = 'parent_process_name'; expression = {(Get-Process -id $_.ppid).name}},
+    #@{name = 'loaded_modules'; expression = {(Get-Process -id $_.pid).Modules.ModuleName -join ','}}
+    @{name = 'module_paths'; expression = {(Get-Process -id $_.pid).Modules.FileName -join ','}}
 
-$netstats | Sort -Property pid | FT -Property proto, process_name, pid, ppid, gpid, parent_process_name, src, dst, state, session_id, window_title
+$netstats | Sort -Property pid | FT -Property proto, window_title, process_name, pid, ppid, src, dst, state, session_id, module_paths -Auto -Wrap
