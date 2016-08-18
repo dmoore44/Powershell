136208196aee28274c659c6016d7e3b988de5ec7
Dallas Moore <Dallas.moore21@gmail.com>
2016-08-18 11:03:27 -0500

    Update Get-CR.ps1

--- Get-CR.ps1
+++ Get-CR.ps1
@@ -1,10 +1,13 @@
 $OSarch = [Environment]::Is64BitOperatingSystem
 
 If ($OSarch -eq $true){
-    $ScriptURL = "https://raw.githubusercontent.com/dmoore44/Powershell/master/CR32.ps1"
-    }
-Else {
     $ScriptURL = "https://raw.githubusercontent.com/dmoore44/Powershell/master/CR64.ps1"
+    }
+ElseIf ($OSarch -eq $false) {
+    $ScriptURL = "https://raw.githubusercontent.com/dmoore44/Powershell/master/CR32.ps1"
+}
+Else {
+$null
 }
 
-Invoke-Expression ((New-Object Net.WebClient).DownloadString("$ScriptURL"))
+Invoke-Expression (New-Object Net.WebClient).DownloadString("$ScriptURL")
